9418f7be6b49db1ce462a3b4501ddaabaf291399
Pooja Raghmode <poojaraghmode05@gmail.com>
2021-04-24 20:26:06 +0530

    Babeling and Reacting:)

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', {root : __dirname});
 });
 
+app.use(express.static(__dirname + "/dist"));
+
 let port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(__dirname);
